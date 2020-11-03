0a2b14c1671df8df56035b22a770586184de160b
Dirac Murairi <diracm8@gmail.com>
2020-11-03 10:02:45 +0200

    create a program that change o to a

---
+++ change_o.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+
+def change_o_to_a(str):
+
+    """
+    This function takes a string as argument
+    It changes every o to a and return a new string.
+    """
+    lst = [] # list that will hold all letters.
+    if not str or type(str) == int:
+        sys.exit()
+    else:
+        for letter in str:
+            if letter == "o":
+                letter = "a"
+            elif letter == "O":
+                letter == "A"
+            lst.append(letter)
+        new_str = ''.join(lst)
+
+    return new_str
+
+user_string = "Dora"
+print(change_o_to_a(user_string))
+print(change_o_to_a("Hello word"))
+print(change_o_to_a(12345))
+print(change_o_to_a(0))
